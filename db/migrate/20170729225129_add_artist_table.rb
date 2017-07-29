8c5efcb8d0c01aff37204c6eca355756173027be
nanark <nanark@gmail.com>
2017-07-30 01:51:59 +0200

    Remove index.

--- db/migrate/20170729225129_add_artist_table.rb
+++ db/migrate/20170729225129_add_artist_table.rb
@@ -3,7 +3,6 @@ class AddArtistTable < ActiveRecord::Migration[5.1]
     create_table "artists", id: :bigint, auto_increment: true, default: nil, force: :cascade, options: "ENGINE=InnoDB ROW_FORMAT=dynamic DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
       t.string "name"
       t.timestamps
-      t.index ["name"], name: "name", using: :btree, unique: true
     end
   end
 end
