69cd67bbff5de648348022a7f3ba905f6b0a3c4a
nanark <nanark@gmail.com>
2017-07-30 09:56:39 +0200

    AA: Adding links to artists.

--- app/admin/playlist.rb
+++ app/admin/playlist.rb
@@ -16,7 +16,12 @@ ActiveAdmin.register Playlist do
       div style: "display:flex; align-items: center;" do
         div auto_link(track, image_tag(track.cover.url(:xsmall), size: 50, style: "margin-right: 10px;"))
         div do
-          div b track.artist if track.artist
+          artist = Artist.find_by(name: track.artist)
+          if artist
+            div b auto_link artist, track.artist
+          else
+            div b track.artist if track.artist
+          end
           div do
             track_title = !track.title.blank? ? track.title : track.track_file_name
             auto_link track, track_title
