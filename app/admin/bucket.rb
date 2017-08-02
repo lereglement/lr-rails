6a53ad838ad1bf5a60093635533c64b180a2f607
nanark <nanark@gmail.com>
2017-08-02 08:49:13 +0200

    AA: Bucket.

--- app/admin/bucket.rb
+++ app/admin/bucket.rb
@@ -1,5 +1,53 @@
 ActiveAdmin.register Bucket do
   config.batch_actions = false
 
+  actions :index
+
+  index do
+    column :id
+    column :track do |bucket|
+      track = bucket.track
+      artist = Artist.find_by(name: track.artist)
+      div style: "display:flex; align-items: center;" do
+        if !track.cover.blank?
+          cover = track.cover.url(:xsmall)
+        elsif artist
+          cover = artist.picture.url(:xsmall)
+        else
+          cover = track.cover.url(:xsmall)
+        end
+        div auto_link(track, image_tag(cover, size: 50, style: "margin-right: 10px;"))
+        div do
+          if artist
+            div b auto_link artist, track.artist
+          else
+            div b track.artist if track.artist
+          end
+          div do
+            track_title = !track.title.blank? ? track.title : track.track_file_name
+            auto_link track, track_title
+          end
+        end
+      end
+    end
+    column :duration do |bucket|
+      Time.at(bucket.track.duration).utc.strftime("%M:%S") if bucket.track.duration
+    end
+    column :last_from_artist do |item|
+      div do
+        before = Playlist.joins("INNER JOIN tracks ON tracks.id = playlists.track_id").where(is_aired: true).where("tracks.artist = ?", item.track.artist).where.not(aired_at: nil).where("playlists.id < ?", item.id).order("playlists.id DESC").first
+        unless before.blank?
+          div DateLib.humanize(Time.now - before.aired_at)
+        end
+      end
+    end
+    column :created do |item|
+      time_ago(item.created_at)
+    end
+    actions
+  end
+
+
+
 
 end
