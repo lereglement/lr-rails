f939f275bef78e1364d005af081d730702b60ba0
nanark <nanark@gmail.com>
2017-08-18 23:33:36 +0200

    OBS: svg to png.

--- app/assets/javascripts/obs/tracks_current.js
+++ app/assets/javascripts/obs/tracks_current.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
       $.get(urlApi + "/v1/playlists/current", function( data ) {
         var track = data.data.title
         if (data.data.is_new) {
-          track = track + ' <img src="https://s3-eu-west-1.amazonaws.com/lereglement-prod/static/tags/new.svg" class="tag">';
+          track = track + '<img src="https://s3-eu-west-1.amazonaws.com/lereglement-prod/static/tags/new.png" class="tag">';
         }
         if (data.data.is_mif) {
-          track = track + ' <img src="https://s3-eu-west-1.amazonaws.com/lereglement-prod/static/tags/mif.svg" class="tag">';
+          track = track + '<img src="https://s3-eu-west-1.amazonaws.com/lereglement-prod/static/tags/mif.png" class="tag">';
         }
         $( "#title" ).html( track );
         $( "#artist" ).html( data.data.artist );
