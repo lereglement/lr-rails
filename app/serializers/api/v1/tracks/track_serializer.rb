d307132c1ae62ec02610133ba22da61730d11298
nanark <nanark@gmail.com>
2017-08-18 15:45:29 +0200

    API: Add cover to current.

--- app/serializers/api/v1/tracks/track_serializer.rb
+++ app/serializers/api/v1/tracks/track_serializer.rb
@@ -3,4 +3,52 @@ class Api::V1::Tracks::TrackSerializer < ActiveModel::Serializer
   attributes :title,
     :artist
 
+    attribute :cover_xsmall do
+      track = object
+      unless track.cover.blank?
+        track.cover.url(:xsmall)
+      else
+        artist = Artist.where(name: track.artist).first
+        unless artist.picture.blank?
+          artist.picture.url(:xsmall)
+        end
+      end
+    end
+
+    attribute :cover_small do
+      track = object
+      unless track.cover.blank?
+        track.cover.url(:small)
+      else
+        artist = Artist.where(name: track.artist).first
+        unless artist.picture.blank?
+          artist.picture.url(:small)
+        end
+      end
+    end
+
+    attribute :cover_medium do
+      track = object
+      unless track.cover.blank?
+        track.cover.url(:medium)
+      else
+        artist = Artist.where(name: track.artist).first
+        unless artist.picture.blank?
+          artist.picture.url(:medium)
+        end
+      end
+    end
+
+    attribute :cover_large do
+      track = object
+      unless track.cover.blank?
+        track.cover.url(:large)
+      else
+        artist = Artist.where(name: track.artist).first
+        unless artist.picture.blank?
+          artist.picture.url(:large)
+        end
+      end
+    end
+
 end
