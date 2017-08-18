7fbfc500a41794b5014c3ab8ea3cf676d9d99056
nanark <nanark@gmail.com>
2017-08-18 23:14:42 +0200

    OBS: Add new + mif tag

--- app/assets/javascripts/obs/tracks_current.js
+++ app/assets/javascripts/obs/tracks_current.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
 
   var interval = self.setInterval(function(){
       $.get(urlApi + "/v1/playlists/current", function( data ) {
-        $( "#title" ).html( data.data.title );
+        var track = data.data.title
+        if (data.data.is_new) {
+          track = track + ' <img src="https://s3-eu-west-1.amazonaws.com/lereglement-prod/static/tags/new.svg" class="tag">';
+        }
+        if (data.data.is_mif) {
+          track = track + ' <img src="https://s3-eu-west-1.amazonaws.com/lereglement-prod/static/tags/mif.svg" class="tag">';
+        }
+        $( "#title" ).html( track );
         $( "#artist" ).html( data.data.artist );
       });
   }, 3000);
