65b683ff7b634ab89793a65d808de8181f526ab2
Thomas Chrétien <thomas@chretien.co>
2017-08-26 20:59:29 +0200

    navbar

--- app/controllers/landing/welcome_controller.rb
+++ app/controllers/landing/welcome_controller.rb
@@ -5,6 +5,24 @@ class Landing::WelcomeController < Landing::BaseController
     set_meta_tags default_meta_tags
     set_meta_tags title: "REGLEMENT.SALE"
     @current_track = Track.get_current
+    @networks = [
+      {
+        slug: 'facebook',
+        link: 'facebook.com/lereglement/'
+      },
+      {
+        slug: 'instagram',
+        link: 'https://www.instagram.com/le_reglement/'
+      },
+      {
+        slug: 'youtube',
+        link: 'https://www.youtube.com/channel/UCcqe1EHmdaLVBwfqk5OtmXw'
+      },
+      {
+        slug: 'twitter',
+        link: 'https://twitter.com/lereglement'
+      }
+    ]
     render layout: '../landing/layout'
   end
 
