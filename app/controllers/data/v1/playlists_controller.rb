c9f1a2577924a400886c6f28f6d8780746a9874f
nanark <nanark@gmail.com>
2017-08-26 20:33:48 +0200

    Track.get_previous(limit)

--- app/controllers/data/v1/playlists_controller.rb
+++ app/controllers/data/v1/playlists_controller.rb
@@ -1,6 +1,6 @@
 class Data::V1::PlaylistsController < Data::V1::BaseController
 
-  skip_before_action :authenticate_request!, only: [:get_current]
+  skip_before_action :authenticate_request!, only: [:get_current, :get_previous]
 
   def get_current
     current_track = Track.get_current
@@ -8,6 +8,14 @@ class Data::V1::PlaylistsController < Data::V1::BaseController
     render json: current_track,
       root: 'data',
       serializer: Data::V1::Tracks::TrackSerializer
+  end
+
+  def get_previous
+    limit = params.fetch(:limit, 10)
+
+    render json: Track.get_previous(limit),
+      root: 'data',
+      each_serializer: Data::V1::Tracks::TrackSerializer
   end
 
 
