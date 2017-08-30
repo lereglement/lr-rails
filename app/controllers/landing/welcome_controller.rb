a6a9d0ce6810ad40395c9e276586950ec3ee4580
Thomas Chrétien <thomas@chretien.co>
2017-08-30 21:24:31 +0200

    list mode

--- app/controllers/landing/welcome_controller.rb
+++ app/controllers/landing/welcome_controller.rb
@@ -15,7 +15,7 @@ class Landing::WelcomeController < Landing::BaseController
       current_track_response = Net::HTTP.get(current_track_uri)
       @current_track = OpenStruct.new(JSON.parse(current_track_response)['data'])
 
-      playlist_url = 'http://data.lereglement.sale/v1/youtube_videos/playlist'
+      playlist_url = 'http://data.lereglement.sale/v1/youtube_videos/playlist?q=analyse'
       playlist_uri = URI(playlist_url)
       playlist_response = Net::HTTP.get(playlist_uri)
       @videos = JSON.parse(playlist_response)['data'].map! do |video|
