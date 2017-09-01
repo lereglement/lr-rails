6b26e30d76aecc09cbb9c03cb92f6cd964704e21
Thomas Chrétien <thomas@chretien.co>
2017-09-01 18:44:22 +0200

    fix btn

--- app/assets/javascripts/header_radio.js
+++ app/assets/javascripts/header_radio.js
@@ -3,12 +3,23 @@ $(document).ready(function() {
   setInterval(function() {
     $.get('http://data.lereglement.sale/v1/playlists/current', function(response) {
       var track = response.data;
-      if(track.title != $('.header-radio-title').text()) {
+      if(track.title != $('.header-radio-title').text() ) {
         $('.cover-huge, .cover-shadow').css({backgroundImage: "url(" + track.cover_large + ")"})
         $('.header-radio-title').text(track.title)
         $('.header-radio-subtitle').html('<span>de</span>  ' + track.artist)
-        $('.track-item').first().remove()
-        $('.previous-tracks').append("<div class='track-item'><div class='track-item-cover' style='background-image: url( " + old_track.cover_xsmall + ")'></div><div><div class='track-item-title'>" + old_track.title + "</div><div class='track-item-artist'>" + old_track.artist + "</div><div></div>")
+
+        if(track.external_source) {
+          $('.button-provider').removeClass('is-hidden button-soundcloud button-youtube')
+          $('.button-provider').addClass('button-' + track.origin_external_source)
+          $('.button-provider').attr('href', track.external_source)
+        } else {
+          $('.button-provider').addClass('is-hidden')
+        }
+
+        if(old_track.title != $('.track-item').last().find('.track-item-title').text()) {
+          $('.track-item').first().remove()
+          $('.previous-tracks').append("<div class='track-item'><div class='track-item-cover' style='background-image: url( " + old_track.cover_xsmall + ")'></div><div><div class='track-item-title'>" + old_track.title + "</div><div class='track-item-artist'>" + old_track.artist + "</div><div></div>")
+        }
 
       } else {
         old_track = track
