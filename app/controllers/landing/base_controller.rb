086a0598eee77a7fae689eb9790248b989cec8af
nanark <nanark@gmail.com>
2017-09-01 17:46:00 +0200

    Meta.

--- app/controllers/landing/base_controller.rb
+++ app/controllers/landing/base_controller.rb
@@ -4,7 +4,7 @@ class Landing::BaseController < ApplicationController
 
     def default_meta_tags
       {
-        title: 'Le Reglement',
+        title: 'Le Règlement',
         description: "",
         charset: "utf-8",
         viewport: "width=device-width, initial-scale=1",
@@ -25,17 +25,17 @@ class Landing::BaseController < ApplicationController
           { rel: "apple-touch-icon-precomposed", sizes: '152x152', href: '/apple-touch-icon-152x152.png' },
         ],
         "application-name": 'lereglement',
-        "msapplication-TileColor": '#32e4c6',
+        "msapplication-TileColor": '#000',
         "msapplication-square70x70logo": '/mstile-70x70.png',
         "msapplication-square150x150logo": '/mstile-150x150.png',
         "msapplication-wide310x150logo": '/mstile-310x150.png',
         "msapplication-square310x310logo": '/mstile-310x310.png',
-        "google-site-verification": '',
-        "fb:app_id": '289683661390900',
+        "google-site-verification": 'YZILL3klorfuwOSOU1fBeaTKELpjooCKSMJnIIGoC1o',
+        # "fb:app_id": '289683661390900',
         og: {
-          title: 'lereglement',
+          title: 'Le Règlement',
           type: 'website',
-          description: 'lereglement is a venture capital mutant. We love startups, enthusiasm and cookies.',
+          description: 'Ceux qui respectent le règlement ne le respectent pas correctement.',
           url: 'https://lereglement.sale',
           image: [{
             _: 'https://lereglement.sale/tile-facebook.png',
@@ -45,8 +45,8 @@ class Landing::BaseController < ApplicationController
         },
         twitter: {
           card: "summary",
-          site: "@lereglementpolis",
-          description: "lereglement is a venture capital mutant. We love startups, enthusiasm and cookies.",
+          site: "@lereglement",
+          description: 'Ceux qui respectent le règlement ne le respectent pas correctement.',
           image: {
             _: "https://lereglement.sale/tile.png",
             width: 558,
