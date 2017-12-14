aa3fb6fbe77f6e5ec75f8b293dc9a27205485d0b
Loic CHOLLIER <loic@chollier.com>
2017-12-14 13:42:49 +0100

    add roles and access to track create/edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214100957) do
+ActiveRecord::Schema.define(version: 20170918203822) do
 
   create_table "active_admin_comments", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "namespace"
@@ -162,7 +162,6 @@ ActiveRecord::Schema.define(version: 20171214100957) do
     t.string "state", limit: 40, default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "roles_mask"
     t.index ["facebook_user_id"], name: "facebook_user_id", unique: true
     t.index ["ref"], name: "ref", unique: true
     t.index ["state"], name: "state"
