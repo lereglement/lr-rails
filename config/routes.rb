3a24e60e2cfc6457d4ccd2aea453f44e96a2d850
Jonathan Pepin <jypepin@gmail.com>
2018-09-01 17:14:43 +0200

    remove all routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,6 @@ Rails.application.routes.draw do
     get ':any', to: redirect(subdomain: nil, path: '/%{any}'), any: /.*/
   end
 
-  constraints(lambda { |req| req.host.match(/^data(-staging)?\.lereglement\.(here|sale)$/) }) do
-    namespace :data, :path => '/' do
-      namespace :v1 do
-        get :get_youtube_playlist, path: '/youtube_videos/playlist', to: 'youtube_videos#get_playlist'
-        get :get_current_track, path: '/playlists/current', to: 'playlists#get_current'
-        get :get_previous_tracks, path: '/playlists/previous', to: 'playlists#get_previous'
-        resources :facebook, only: [:create, :show]
-        resources :settings, only: [:index]
-      end
-    end
-  end
-
   namespace :landing, :path => '/' do
     get :welcome, path: '/', to: 'welcome#index'
   end
