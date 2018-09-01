63b5ca162f32276302f58240a85bd3b1c5fde91d
Jonathan Pepin <jypepin@gmail.com>
2018-09-01 17:14:00 +0200

    remove radio from website

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,49 @@
 module ApplicationHelper
+  def get_youtube_videos(params)
+    q = params.fetch(:q, nil)
+    results = params.fetch(:results, 50)
+    remove_live = params.fetch(:remove_live, 0)
+
+    url = "https://www.googleapis.com/youtube/v3/search?key=#{Rails.application.secrets.youtube_api_key}&channelId=#{Rails.application.secrets.youtube_channel_id}&part=id,snippet&type=video&order=date&maxResults=#{results}&q=#{q}"
+    http = Curl.get(url)
+    api_error(status: 500, errors: "Youtube API not responding") and return false if !http.body_str
+
+    body = JSON.parse(http.body_str)
+    api_error(status: 500, errors: "Youtube API items missing") and return false if !body["items"]
+
+    ids = []
+    body["items"].each do |item|
+      if item["snippet"]["liveBroadcastContent"] != "live" || remove_live.to_i == 0
+        ids.push(item["id"]["videoId"])
+      end
+    end
+
+    url = "https://www.googleapis.com/youtube/v3/videos?key=#{Rails.application.secrets.youtube_api_key}&channelId=#{Rails.application.secrets.youtube_channel_id}&part=snippet,statistics&id=#{ids.join(',')}"
+    http = Curl.get(url)
+    api_error(status: 500, errors: "Youtube API not responding") and return false if !http.body_str
+
+    body = JSON.parse(http.body_str)
+    api_error(status: 500, errors: "Youtube API items missing") and return false if !body["items"]
+
+    playlist = []
+    body["items"].each do |item|
+      playlist.push({
+        id: item["id"],
+        url: "https://www.youtube.com/watch?v=#{item["id"]}",
+        title: item["snippet"]["title"],
+        statistics: {
+          view_count: item["statistics"]["viewCount"],
+          like_count: item["statistics"]["likeCount"],
+          dislike_count: item["statistics"]["dislikeCount"],
+          comment_count: item["statistics"]["commentCount"],
+        },
+        thumbnail: {
+          url: item["snippet"]["thumbnails"]["high"]["url"],
+          width: item["snippet"]["thumbnails"]["high"]["width"],
+          height: item["snippet"]["thumbnails"]["high"]["height"],
+        }
+      })
+    end
+    return playlist
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,29 +4,6 @@ Rails.application.routes.draw do
     get ':any', to: redirect(subdomain: nil, path: '/%{any}'), any: /.*/
   end
 
-  constraints(lambda { |req| req.host.match(/^obs(-staging)?\.lereglement\.(here|xyz)$/) }) do
-    namespace :obs, :path => '/' do
-      get :track, path: '/tracks/current', to: 'tracks#current'
-    end
-  end
-
-  constraints(lambda { |req| req.host.match(/^bo(-staging)?\.lereglement\.(here|xyz)$/) }) do
-    devise_for :admin_users, ActiveAdmin::Devise.config
-    ActiveAdmin.routes(self)
-  end
-
-  constraints(lambda { |req| req.host.match(/^api(-staging)?\.lereglement\.(here|xyz)$/) }) do
-    namespace :api, :path => '/' do
-      namespace :v1 do
-        get :get_next_track, path: '/playlists/next', to: 'playlists#get_next'
-        get :get_current_track, path: '/playlists/current', to: 'playlists#get_current'
-        get :get_not_converted, path: '/tracks/not_converted', to: 'tracks#get_not_converted'
-        get :get_not_downloaded, path: '/tracks/not_downloaded', to: 'tracks#get_not_downloaded'
-        resources :tracks, only: [:create, :update]
-      end
-    end
-  end
-
   constraints(lambda { |req| req.host.match(/^data(-staging)?\.lereglement\.(here|sale)$/) }) do
     namespace :data, :path => '/' do
       namespace :v1 do
@@ -36,12 +13,6 @@ Rails.application.routes.draw do
         resources :facebook, only: [:create, :show]
         resources :settings, only: [:index]
       end
-    end
-  end
-
-  constraints(lambda { |req| req.host.match(/^(staging\.)?lereglement\.(here|sale)$/) }) do
-    namespace :landing, :path => '/' do
-      get :welcome, path: '/', to: 'welcome#index'
     end
   end
 
